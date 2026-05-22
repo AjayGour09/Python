74fa6e6f89650f7e8f42701e5ba1d800869c2c02
Ajay Gour <ajaygour369@gmail.com>
2026-05-22 10:34:27 +0530

    learning loops and do some practice

---
+++ Chapter_07/loops.py
@@ -0,0 +1,13 @@
+# i = 1;
+# while(i<10):
+#     print(i)
+#     i+=1
+
+list = ["Rohan" , "Sumit" , "Vinod","Gopal"]
+# i =0
+# while(i<len(list)):
+#     print(list[i])
+#     i = i+1
+
+for i in list:
+    print(i)
---
+++ Chapter_07/practice.py
@@ -0,0 +1,39 @@
+# num = int (input ("Enter Your numberc : "))
+
+# for i in range( 1, 11):
+#     print(f"{num} * {i} = {num*i}")
+
+
+
+
+# list = ["Harry","Rohan" ,"Sohan","Mohit"]
+
+# for name in list:
+#     if(name.startswith("S")):
+#         print(f"Hello {name}")
+   
+
+# num = int (input("Enter number :"))
+
+# for i in range(2 ,num):
+#     if(num%i) == 0:
+#         print("Number is not prime")
+#         break
+# else:
+#     print("Number is Prime")
+
+
+# n = int (input("Enter Number :"))
+
+# i = 1;
+# sum = 0;
+# while(i<=n):
+#     sum += i
+#     i += 1
+# print(sum)
+
+num =int(input("Enter number : "))
+product = 1 ;
+for i in range(1, num + 1):
+    product = product * i
+print(f"The Factorial of 3{num} is {product}")
