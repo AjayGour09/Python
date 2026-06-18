c53aba51c0b0f4fea443b20e6260137981d89a96
Ajay Gour <ajaygour369@gmail.com>
2026-06-18 17:33:27 +0530

    add seaborn and matplotlib visualizations for outlier detection

--- seaborn/outliers.py
+++ seaborn/outliers.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 arr = np.array([1,2,3,4,5,6,7,8,30])
 meadian = 5
@@ -18,3 +20,6 @@ arr2 = np.array(l)
 print(arr2)
 
 print(f"The Q1 is {q1} and Q3 is {q3} and Median is {meadian} and IQR is {Iqr} and lower value is {lower} and upper value is {uper} and the outlier is 30")
+print(sns.boxplot( x = arr))
+print(sns.boxplot( x = arr2))
+print(plt.show())
